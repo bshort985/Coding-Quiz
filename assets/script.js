459a80f1a5e87dbe67a27bb52dcec403f39bf8cf
bshor <bshort985@gmail.com>
2022-03-09 20:08:13 -0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+//create an array to hold the questions
+//crate a function to start the quiz
+//create local storage to hold the high scores
+//create a function to start next question
+//create a function to select a answer
